3350282c6e4a8dd388b1d9db5a1507bbdddb9384
Prof. Percival <110649262+Prof-Percival@users.noreply.github.com>
2023-05-26 06:03:04 +0200

    ops math

--- free_stack.c
+++ free_stack.c
@@ -1,19 +1,32 @@
 #include "monty.h"
-
 /**
- *  get_free - Free stack
- *
- *  @head: Stack pointer
- *
- *  Return: Void
+ *FREE_STACK - frees all elements of stack
+ *@stack: a pointer to the head node
  */
 
-
-void get_free(stack_t *head)
+void FREE_STACK(stack_t **stack)
 {
-	if (head)
+	stack_t *node = NULL;
+
+	if (!stack || !*stack)
+		return;
+	while (*stack)
 	{
-		get_free(head->next);
-		free(head);
+		node = *stack;
+		*stack = (*stack)->next;
+		free(node);
 	}
+	*stack = NULL;
+}
+
+/**
+ *sub_err- handles error for op_sub
+ *@stack: a ptr to head node
+ *@line_number: a line wear error occured
+ */
+void sub_err(stack_t **stack, unsigned int line_number)
+{
+	fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+	FREE_STACK(stack);
+	exit(EXIT_FAILURE);
 }
--- main.c
+++ main.c
@@ -1,53 +1,53 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
- * main - bytecode interpreter
+ * main - entry point
  * @argc: argument count
- * @argv: bytecode directory
- * Return: 0
+ * @argv: argument variable
+ * Return: 0 on success
  */
-int main(int argc, char *argv[])
+
+struct Montycontext context = {NULL, NULL, NULL, 0};
+
+int main(int argc, char **argv)
 {
-    char *content = NULL;
-    FILE *file;
-    size_t size = 0;
-    ssize_t read_line;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
+	FILE *file_ptr;
+	ssize_t byte_read = 0, lin_buf = 0, line_len = 0, line_num = 1;
+	char *line_read = NULL;
+	char op_code[MAX], op_value[MAX];
+	stack_t *STACK = NULL, *tmp;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2 || !argv[1])
+		usage_err();
 
-    file = fopen(argv[1], "r");
-    bus.file = file;
+	context.stack_type = 0;
+	context.op_file = argv[1];
+	file_ptr = fopen(context.op_file, "r");
 
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (!file_ptr)
+		file_err();
+	while ((byte_read = getline(&line_read, &lin_buf, file_ptr)) != -1)
+	{
+		line_len = sscanf(line_read, "%s %s", op_code, op_value);
+		if (line_len > 2)
+		{
+			free(line_read);
+			line_read = NULL;
+			FREE_STACK(&STACK);
+			usage_err();
+		}
+		line_num++;
+		context.ext_code = op_code;
+		context.ext_value = op_value;
 
-    while ((read_line = getline(&content, &size, file)) != -1)
-    {
-        bus.content = content;
-        counter++;
-
-        if (read_line > 0)
-        {
-            execute(content, &stack, counter, file);
-        }
-    }
-
-    free(content);
-    free_stack(stack);
-    fclose(file);
-    return (0);
+		if (line_len > 0 && op_code[0] != '#')
+			get_func(context.ext_code)(&STACK, line_num);
+		memset(op_code, '\0', sizeof(op_code));
+		memset(op_value, '\0', sizeof(op_value));
+		free(line_read);
+		line_read = NULL;
+	}
+	free(line_read);
+	FREE_STACK(&STACK);
+	fclose(file_ptr);
+	return (0);
 }
-
